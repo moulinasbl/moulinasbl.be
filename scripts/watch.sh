8253ad3190aa3e5be9dacb8867f010e335494b62
Quentin Delcourt <kant312@gmail.com>
2017-11-11 17:46:14 +0100

    Add browser-sync to watch

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -1,1 +1,1 @@
-nodemon -w public/assets/src/css -x "npm run styles" & nodemon -w public/assets/src/js -x "npm run js" & nodemon -w public/assets/src/images -x "npm run images" & nodemon -w public/assets/src/fonts -x "npm run fonts"
+nodemon -w public/assets/src/css -x "npm run styles" & nodemon -w public/assets/src/js -x "npm run js" & nodemon -w public/assets/src/images -x "npm run images" & nodemon -w public/assets/src/fonts -x "npm run fonts" & browser-sync start -p http://moulinasbl.loc --no-open -f "craft/templates/**/*.html,public/assets/dist/**/*"
