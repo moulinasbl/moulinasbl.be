1a9ef3c554d9e236bd46854510e3842a5ad2f642
Quentin Delcourt <kant312@gmail.com>
2017-12-02 10:41:50 +0100

    Add current selection to menu

--- craft/translations/fr.php
+++ craft/translations/fr.php
@@ -1,9 +1,11 @@
 <?php
 
 return [
+    'Read more'                         => 'Lire la suite',
     'See our activities'                => 'Voir nos activités',
     'Discover the team'                 => 'Découvrez l’équipe',
     'When'                              => 'Quand',
     'Where'                             => 'Où',
     'From {dateStart} to {dateEnd}'     => 'Du {dateStart} au {dateEnd}',
+    'There are no upcoming activities at the moment...'     => 'Il n’y a pas d’activité prévue en ce moment...',
 ];
